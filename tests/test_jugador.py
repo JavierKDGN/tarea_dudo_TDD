21e8cfd7f8053c41286d78a3b53a9566179b05dc
JavierKDGN <javier.cadagan6@gmail.com>
2025-08-31 19:13:58 -0400

    GREEN: Implementacion minima para [creacion de jugador con 5 dados iniciales]. Se crea una instancia de Jugador, la cual tiene un nombre y se le inyecta un Cacho. Este Cacho no esta implementado actualmente por lo que se Mockea.

--- tests/test_jugador.py
+++ tests/test_jugador.py
@@ -1,5 +1,11 @@
 import pytest
+from src.game.jugador import Jugador
 
-def test_jugador_inicia_con_5_dados():
-    jugador = Jugador()
+def test_jugador_inicia_con_5_dados(mocker):
+    # Creamos un Mock del cacho del jugador
+    mock_cacho_jugador = mocker.Mock()
+    # El Cacho inicia con 5 dados al empezar la partida
+    mock_cacho_jugador.contar_dados.return_value = 5
+
+    jugador = Jugador(nombre="test", cacho = mock_cacho_jugador)
     assert jugador.get_cantidad_dados() == 5
