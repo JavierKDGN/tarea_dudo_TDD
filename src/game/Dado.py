d834017e7bb1eb1032e9a60fd5c51928d721b7ca
Diego Rebollo <diego-rebollo-garcia@proton.me>
2025-08-31 19:48:30 -0400

    [FEATURE](~GREEN~): Implement `Dado` name functionality

--- src/game/Dado.py
+++ src/game/Dado.py
@@ -1,19 +1,33 @@
 import random
 
 class Dado:
+	__name = None
 	__value = None
 	
 	__MIN_VALUE = 1
 	__MAX_VALUE = 6
 	
+	__NAMES = {
+		1: 'As',
+		2: 'Tonto',
+		3: 'Tren',
+		4: 'Cuadra',
+		5: 'Quina',
+		6: 'Sexto'
+	}
+	
 	def __init__(self):
 		self.roll()
+	
+	def getName(self):
+		return self.__name
 	
 	def getValue(self):
 		return self.__value
 	
 	def setValue(self, value):
 		self.__value = value
+		self.__name = self.__NAMES[self.__value]
 	
 	def roll(self):
 		self.setValue(random.randint(self.__MIN_VALUE, self.__MAX_VALUE))
