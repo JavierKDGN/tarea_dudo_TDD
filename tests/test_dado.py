067b220ae2a75e0d9f3a63da87fa3f06c2c4f4ef
Diego Rebollo <diego-rebollo-garcia@proton.me>
2025-08-31 19:11:03 -0400

    [FEATURE](~RED~): Write test for `Dado` new value generation
    	Tests: Fail as expected

--- tests/test_dado.py
+++ tests/test_dado.py
@@ -4,3 +4,10 @@ def test_value():
 	dado = Dado()
 	
 	assert (dado.value >= 1) and (dado.value <= 6)
+
+def test_reroll():
+	dado = Dado()
+	
+	dado.reroll()
+	
+	assert (dado.value >= 1) and (dado.value <= 6)
