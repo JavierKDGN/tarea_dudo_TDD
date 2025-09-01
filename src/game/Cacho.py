bb49b8a387b724cbbffd214ff53ba24f2d349969
Diego Rebollo <diego-rebollo-garcia@proton.me>
2025-08-31 21:35:06 -0400

    [FEATURE](~GREEN~): Implement `Cacho` insertion and removal functionality

--- src/game/Cacho.py
+++ src/game/Cacho.py
@@ -10,6 +10,12 @@ class Cacho:
 	def getDados(self):
 		return self.__dados
 	
+	def insertDado(self):
+		self.__dados.append(Dado())
+	
+	def removeDado(self):
+		self.__dados.pop()
+	
 	def shake(self):
 		for i in range(0, len(self.__dados)):
 			self.__dados[i].roll()
