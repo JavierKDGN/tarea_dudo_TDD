5b0d6b77d7d89a8c2f13f15998179abcd29a5193
JavierKDGN <javier.cadagan6@gmail.com>
2025-08-31 22:52:58 -0400

    [RED]: Test de [GeneradorAleatorio] para generar numeros al azar o deterministas segun la condicion

--- tests/test_rng.py
+++ tests/test_rng.py
@@ -2,6 +2,6 @@ import pytest
 from src.tools.rng import GeneradorAleatorio
 
 def test_generar_num_azar():
-    rng = GeneradorAleatorio(determinista=False)
+    rng = GeneradorAleatorio()
     num = rng.generar(min_num=1, max_num=6)
     assert num in range(1,7)
