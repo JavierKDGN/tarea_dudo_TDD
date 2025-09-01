c3cbb0f34bf940f4fa24a8aa30df50be89c63012
JavierKDGN <javier.cadagan6@gmail.com>
2025-08-31 23:03:09 -0400

    [Green]: Implementacion de [GeneradorDeterminista], genera siempre el mismo numero

--- src/tools/rng.py
+++ src/tools/rng.py
@@ -5,6 +5,10 @@ class GeneradorAleatorio:
     def generar(self, min_num:int, max_num:int):
         return random.randint(min_num, max_num)
 
+class GeneradorDeterminista:
+    numero = 1
 
+    def generar(self, min_num:int, max_num:int):
+        return self.numero
 
 
