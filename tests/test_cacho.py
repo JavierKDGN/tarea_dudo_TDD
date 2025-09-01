fbbeb882f6cb04e6a4d05c0bbdea22d7143af37a
Diego Rebollo <diego-rebollo-garcia@proton.me>
2025-08-31 21:28:48 -0400

    [FEATURE](~RED~): Write test for `Cacho` `shake()` functionality

--- tests/test_cacho.py
+++ tests/test_cacho.py
@@ -4,3 +4,17 @@ def test_initial_content():
 	cacho = Cacho()
 	
 	assert len(cacho.getDados()) == 5
+
+def test_shake():
+	cacho = Cacho()
+	
+	dados1 = cacho.getDados()
+	
+	cacho.shake()
+	
+	dados2 = cacho.getDados()
+	
+	assert len(dados1) == len(dados2)
+	
+	# Cannot test if shake was "successful" because it is absolutely possible
+	# that both lists have the same values after a shake
