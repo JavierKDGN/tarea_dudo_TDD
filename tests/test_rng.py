54f74a874a81bdeed0bec60486cd1888a4a76824
JavierKDGN <javier.cadagan6@gmail.com>
2025-08-31 23:02:39 -0400

    [Red]: Test de [GeneradorDeterminista] para generar siempre el mismo numero en casos de testing

--- tests/test_rng.py
+++ tests/test_rng.py
@@ -1,7 +1,12 @@
 import pytest
-from src.tools.rng import GeneradorAleatorio
+from src.tools.rng import GeneradorAleatorio, GeneradorDeterminista
 
 def test_generar_num_azar():
     rng = GeneradorAleatorio()
     num = rng.generar(min_num=1, max_num=6)
     assert num in range(1,7)
+
+def test_generar_num_determinista():
+    rng = GeneradorDeterminista()
+    num = rng.generar(min_num=1, max_num=6)
+    assert num == rng.numero
