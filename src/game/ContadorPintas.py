8a912a0d7c378bbcae4049047436b689cc184034
Felicia <feliciapino100@gmail.com>
2025-09-02 16:34:11 -0400

    REFACTOR: Reduce anidamiento en [ContadorPintas]

--- src/game/ContadorPintas.py
+++ src/game/ContadorPintas.py
@@ -8,14 +8,6 @@ class ContadorPintas:
                 count += 1
             if d.getValue() == 1:
                 ases += 1
-        if ases_como_comodin:
-            if count != 0:
-                if pinta != 1:
-                    return count+ases
-                else:
-                    return count
-            else:
-                return 0
-        else:
-            return count
+        if (not ases_como_comodin) or pinta==1 or count==0: return count
+        return count + ases
 
