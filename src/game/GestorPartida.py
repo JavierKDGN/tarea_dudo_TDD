fb3c83c74cb79605bdf3f02a47d402bc44369ddb
JavierKDGN <javier.cadagan6@gmail.com>
2025-09-02 00:29:53 -0400

    [Green]: Implementar logica para determinar el turno inicial en [GestorPartida] considerando empates.

--- src/game/GestorPartida.py
+++ src/game/GestorPartida.py
@@ -10,13 +10,36 @@ class GestorPartida:
         return self.jugador_actual
 
     def determinar_turno_inicial(self):
-        max_valor = 0
-        for jugador in self.jugadores:
-            # Cada jugador tira un dado, este no forma parte del cacho ya que es solo para determinar el turno
-            dado = Dado()
-            if dado.getValue() > max_valor:
-                self.jugador_actual = jugador
-                max_valor = dado.getValue()
+        jugadores_dados_map = dict.fromkeys(self.jugadores, 0)
+        max_valor = -1
+        while len(jugadores_dados_map) > 1:
+
+            for jugador in jugadores_dados_map.keys():
+                dado = Dado()
+                jugadores_dados_map[jugador] = dado.getValue()
+
+            jugadores_dados_map = dict(sorted(jugadores_dados_map.items(),key=lambda x:x[1], reverse=True))
+            max_valor = next(iter(jugadores_dados_map.values()))
+
+            perdedores = []
+            for k, v in jugadores_dados_map.items():
+                # Eliminamos los perdedores
+                if v < max_valor:
+                    perdedores.append(k)
+
+            for p in perdedores:
+                jugadores_dados_map.pop(p)
+
+        self.jugador_actual = list(jugadores_dados_map)[0]
+
+
+
+
+
+
+
+
+
 
 
 
