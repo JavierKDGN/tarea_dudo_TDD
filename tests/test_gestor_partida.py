590fc016882a90c35e4d0dec324d88ab51018bcc
JavierKDGN <javier.cadagan6@gmail.com>
2025-09-01 21:47:16 -0400

    [Refactor]: Simplificar aun mas mock de jugadores para los tests del gestor, ahora es un factory con un numero variable

--- tests/test_gestor_partida.py
+++ tests/test_gestor_partida.py
@@ -1,5 +1,4 @@
 import pytest
-import pytest_mock
 from pytest_mock import mocker
 
 from src.game.Cacho import Cacho
@@ -7,17 +6,26 @@ from src.game.Jugador import Jugador
 from src.game.GestorPartida import GestorPartida
 
 @pytest.fixture
-def mock_2_jugadores(mocker):
-    mock_jugador1 = mocker.Mock(nombre = "Jugador1")
-    mock_jugador2 = mocker.Mock(nombre = "Jugador2")
+def mock_jugadores_factory(mocker):
+    '''
+    Al hacer tests me di cuenta que se repetia el mock de jugadores
+    y a veces necesitaba 2 jugadores o mas, por lo que
+    este fixture de pytest resume este proceso y permita ajustar
+    la cantidad de jugador en cada llamada
+    '''
+    def _create(num_jugadores: int, num_dados_por_jugador: int = 5):
+        jugadores = []
+        for i in range(num_jugadores):
+            jugador = mocker.Mock()
+            jugador.nombre = f"Jugador{i+1}"
+            jugador.get_cantidad_dados.return_value = num_dados_por_jugador
+            jugadores.append(jugador)
+        return jugadores
+    return _create
 
-    jugadores = [mock_jugador1, mock_jugador2]
-    return jugadores
 
-
-def test_crear_gestor_con_jugadores(mock_2_jugadores):
-    gestor = GestorPartida(mock_2_jugadores)
+def test_crear_gestor_con_jugadores(mock_jugadores_factory):
+    gestor = GestorPartida(mock_jugadores_factory(2))
 
     assert [p.nombre for p in gestor.jugadores] == ["Jugador1", "Jugador2"]
-
 
