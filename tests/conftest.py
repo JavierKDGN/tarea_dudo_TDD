67c9060491c5a6dedf4aafaec186b5a63982e52f
JavierKDGN <javier.cadagan6@gmail.com>
2025-09-02 20:44:32 -0400

    [Red]: Agregar test para verificar eliminacion de jugador cuando pierde dados en [GestorPartida], fallo. Mock de jugador ajustado para seguir con mas precision el comportamiento de jugador.

--- tests/conftest.py
+++ tests/conftest.py
@@ -13,9 +13,19 @@ def mock_jugadores_factory(mocker):
         for i in range(num_jugadores):
             jugador = mocker.Mock()
             jugador.__nombre = f"Jugador{i+1}"
+
+            dados = [object() for i in range (num_dados_por_jugador)]
+
+            def getDados():
+                return list(dados)
+
+            def removeDado():
+                if dados:
+                    dados.pop(0)
+
             jugador.__cacho = mocker.Mock()
-            jugador.__cacho.getDados.return_value = [None] * num_dados_por_jugador
-            jugador.__cacho.removeDado.return_value = None
+            jugador.__cacho.getDados.side_effect = getDados
+            jugador.__cacho.removeDado.side_effect = removeDado
 
             jugador.get_cacho.return_value = jugador.__cacho
             jugador.get_nombre.return_value = jugador.__nombre
